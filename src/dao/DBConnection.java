c6cba9b5112cbe4dd80d20fb6ab1a35ca117e2a3
Raj Verma <rajver8443@gmail.com>
2025-09-17 23:51:58 +0530

    done

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,14 +1,18 @@
 package dao;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/quizdb?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "quizuser";     // change if needed
-    private static final String PASSWORD = "quizpass"; // change if needed
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/quizdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "quizuser";   // ya "root"
+    private static final String PASSWORD = "quiz123"; // apna password daalo
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // ensures driver available
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -18,5 +22,4 @@ public class DBConnection {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
 
